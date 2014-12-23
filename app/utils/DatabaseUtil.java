a943d505349f6632412e77529e17729dfc0b3b29
yyphlj <514362189@qq.com>
2014-12-23 15:36:23 +0800

    test

--- app/utils/DatabaseUtil.java
+++ app/utils/DatabaseUtil.java
@@ -3,71 +3,43 @@ package utils;
 import java.util.Date;
 
 import models.Baby;
-import models.FetalMovement;
-import models.GestationalWeight;
-import models.Menses;
-import models.Temperature;
 import models.User;
+import models.UserToBaby;
 
 public class DatabaseUtil {
 	public static void init(){
-		System.out.println("database init");
-		
+		System.out.println("数据库初始化");
 		/*人员测试数据*/
-		for(int i=0;i<50;i++){
-			User u = new User();
-			u.userName = "user" + i;
-			u.realName = "测试者" + i;
-			u.passwd = "abcd" + i;
-			u.phoneNum = "1598907064" + i;
-			u.email = "31596495" + i + "@qq.com";
-			u.IDcard = "44138119911024181" + i;
-			u.save();
-		}
-		User u1 = (User) User.find("byUserName", "user1").fetch().get(0);
-		User u2 = (User) User.find("byUserName", "user2").fetch().get(0);
-		
+		User u0 = new User("曾柏羲","12344623","boxiZen","15989070640","boxizen@qq.com","441381199110241816");
+		u0.save();
+		User u1 = new User("测试1","43235242","test0","15989070640","test1@qq.com","441381199110241816");
+		u1.save();
+		User u2 = new User("测试2","67868769","test1","15989070640","test1@qq.com","441381199110241816");
+		u2.save();
+		User u3 = new User("测试3","08900756","test2","15989070640","test2@qq.com","441381199110241816");
+		u3.save();
+		User u4 = new User("测试4","fsdferfw","test3","15989070640","test3@qq.com","441381199110241816");
+		u4.save();
+		User u5 = new User("测试5","94562839","test4","15989070640","test4@qq.com","441381199110241816");
+		u5.save();
+		User u6 = new User("测试6","0fj49fj4","test5","15989070640","test5@qq.com","441381199110241816");
+		u6.save();
+		User u7 = new User("测试7","756g4634","test6","15989070640","test6@qq.com","441381199110241816");
+		u7.save();
+		User u8 = new User("测试8","6575ghf5","test7","15989070640","test7@qq.com","441381199110241816");
+		u8.save();
+		User u9 = new User("测试9","zxcsdgd5","test8","15989070640","test8@qq.com","441381199110241816");
+		u9.save();
 		/*宝贝测试数据*/
-		for(int i=0;i<10;i++){
-			Baby baby = new Baby();
-			baby.userId = u1.id;
-			baby.name = "小喇叭" + i + "号";
-			baby.sex = "male";
-			baby.date = new Date();
-			baby.save();
-		}
+		Baby baby = new Baby(new Date(),"male","小喇叭",u0.id);
+    	baby.save();
+    /*	UserToBaby userBaby = new UserToBaby(u0.id,baby.id);
+    	userBaby.save();*/
+    	System.out.println("用户ID:"+u0.getId());
     	
-    	Menses menses = new Menses(u1.id, new Date(),"暗红", "多", true, true,"稠");
-    	menses.save();
-    	Menses menses2 = new Menses(u1.id, new Date(),"鲜红", "少", true, true,"稀");
-    	menses2.save();
-    	Menses menses3 = new Menses(u2.id, new Date(),"鲜红", "少", true, true,"稀");
-    	menses3.save(); 
-    	
-    	Temperature temperature1 = new Temperature(u1.id,new Date(), 36.7F);
-    	temperature1.save();
-    	Temperature temperature2 = new Temperature(u2.id,new Date(), 37.7F);
-    	temperature2.save();
-    	Temperature temperature3 = new Temperature(u1.id,new Date(), 36.9F);
-    	temperature3.save();
-    	
-<<<<<<< HEAD
-    	GestationalWeight weight1 = new GestationalWeight(u1.id,new Date(), 56.2F);
-    	weight1.save();
-    	GestationalWeight weight2 = new GestationalWeight(u2.id,new Date(), 56.5F);
-    	weight2.save();
-    	GestationalWeight weight3 = new GestationalWeight(u1.id,new Date(), 56.6F);
-    	weight3.save();
-    	
-    	
-    	FetalMovement movement1 = new FetalMovement(u1.id,new Date(), 8);
-    	movement1.save();
-    	FetalMovement movement2 = new FetalMovement(u1.id,new Date(), 6);
-    	movement2.save();
-    	FetalMovement movement3 = new FetalMovement(u1.id,new Date(), 10);
-    	movement3.save();
-    	
-=======
->>>>>>> 806e5b7e2a054b65fd477fe56bf9446066dbbfde
+    	Baby baby1 = new Baby(new Date(),"female","小飞机",u1.id);
+    	baby1.save();
+    	/*UserToBaby userBaby1 = new UserToBaby(u1.id,baby1.id);
+    	userBaby1.save();*/
 	}
 }
