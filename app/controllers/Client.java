6eeded7692f7b0a65a57fc1be8bfca666dd5a6b2
boxizen <boxizen@gmail.com>
2015-05-04 23:20:57 +0800

    添加微信接口

--- app/controllers/Client.java
+++ app/controllers/Client.java
@@ -4,9 +4,18 @@ import java.util.Date;
 import java.util.List;
 
 
+
+import java.util.Map;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 import play.cache.CacheFor;
 import play.db.jpa.Model;
+import play.libs.WS;
+import play.libs.WS.HttpResponse;
 import play.mvc.*;
+import utils.Sign;
 
 /**
  * 客户端控制器
@@ -64,6 +73,41 @@ public class Client extends WebService{
 	 **/
 	public static void vaccine(){
 		render("/Client/record/vaccine.html");
+	}
+	
+	/**
+	 * 获取jsapi_ticket
+	 */
+	public static void medBoxWx(){
+		String timestamp = "";
+		String nonceStr = "";
+		String signature = "";
+		try{
+			/* 获取当前页面*/
+			String crtUrl = "http://clovefemale.boxizen.com/medbox";
+			/* 获取access_token*/
+			models.WeChat wechat = (models.WeChat) models.WeChat.findAll().get(0);
+			String accessToken = wechat.access_token;
+	
+			/* 调用微信接口获取jsapi_ticket */
+			HttpResponse resp = WS.url("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token="+accessToken+"&type=jsapi").get();
+			JsonElement jsonElement = resp.getJson();
+			JsonObject json = jsonElement.getAsJsonObject();
+			String ticket = json.get("ticket").getAsString();
+			Map<String,String> ret = Sign.process(ticket, crtUrl);
+			for (Map.Entry entry : ret.entrySet()) {
+	            System.out.println(entry.getKey() + ", " + entry.getValue());
+	        }
+			timestamp = ret.get("timestamp");
+			nonceStr = ret.get("nonceStr");
+			signature = ret.get("signature");
+		}
+		catch(Exception e){
+			
+		}
+		finally{
+			render("/Client/record/medBox.html",timestamp,nonceStr,signature);
+		}
 	}
 }
 
