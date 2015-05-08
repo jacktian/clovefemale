72866b40dae9f8fd02c0ac62d92010fd409a5abc
boxizen <boxizen@gmail.com>
2015-05-08 18:11:57 +0800

    添加拦截器获取用户openid

--- app/controllers/Client.java
+++ app/controllers/Client.java
@@ -12,10 +12,12 @@ import jobs.AccessTokenRefresher;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import play.Play;
 import play.cache.CacheFor;
 import play.db.jpa.Model;
 import play.libs.WS;
 import play.libs.WS.HttpResponse;
+import play.libs.WS.WSRequest;
 import play.mvc.*;
 import utils.Sign;
 
@@ -26,11 +28,19 @@ import utils.Sign;
  * @since 2015/03/23
  */
 public class Client extends WebService{
-	
+	/* 从配置文件中获取appKey与appSecret */
+	public static String appKey = Play.configuration.getProperty("wechat_appkey");
+	public static String appSecret = Play.configuration.getProperty("wechat_secret");
+	/*用户openid*/
+	public static String openid = null;
+	/* 拦截器 */
 	@Before(unless={"record","first"})
 	public static void getCrtUser(){
 		String code = params.get("code");
-		System.out.println("code:"+code);
+		String requestUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?appid="+appKey+"&secret="+appSecret+"&code="+code+"&grant_type=authorization_code";
+		JsonElement jsonElement = WS.url(requestUrl).get().getJson();
+		JsonObject json = jsonElement.getAsJsonObject();
+		openid = json.get("openid").getAsString();
 	}
 	
 	
@@ -44,8 +54,8 @@ public class Client extends WebService{
 	/**
 	 * 助孕记录
 	 */
-	public static void pregMense(String code){
-		System.out.println("preg code:"+code);
+	public static void pregMense(){
+		System.out.println("用户openid"+openid);
 		render("/Client/record/mense.html");
 	}
 
