e5dcb8f71ba351f37cc8cf54d6dcc71e49628e99
boxizen <boxizen@gmail.com>
2015-05-22 12:40:10 +0800

    添加药箱修改页面

---
+++ app/beans/MedboxBean.java
@@ -0,0 +1,25 @@
+package beans;
+
+/**
+ * 药箱接口
+ *
+ * @author boxiZen
+ * @since 2015/05/22
+ */
+public class MedboxBean {
+	
+	/**
+     * ID
+     */
+	public String id;
+	
+	/**
+     * 名称
+     */
+	public String name;
+	
+	/**
+     * 药品数量
+     */
+	public int count;
+}
---
+++ app/beans/MedicineBean.java
@@ -0,0 +1,45 @@
+package beans;
+
+/**
+ * 药箱接口
+ *
+ * @author boxiZen
+ * @since 2015/05/22
+ */
+public class MedicineBean {
+	
+	/**
+     * ID
+     */
+	public String id;
+	
+	/**
+     * 名称
+     */
+	public String name;
+	
+	/**
+     * 作用
+     */
+	public String function;
+	
+	/**
+     * 生产日期
+     */
+	public String produce;
+	
+	/**
+     * 到期日期
+     */
+	public String deadline;
+	
+	/**
+     * 条形码
+     */
+	public String code;
+	
+	/**
+     * boxId
+     */
+	public String medicineBoxId;
+}
