5c1cf03e23bbf78b3e7e816b7f86430bf05399e8
boxizen <boxizen@gmail.com>
2015-06-04 15:25:10 +0800

    添加图文链接

--- app/controllers/WeChat.java
+++ app/controllers/WeChat.java
@@ -162,14 +162,8 @@ public class WeChat extends WebService{
 						a1.picUrl = "http://ec8.images-amazon.com/images/I/71i7rL8F46L._SL500_AA300_.png";
 						a1.url = "http://www.baidu.com/link?url=PQul1MKBtDqPgHh0qaQBv2HKRUeS0k4b2sYuEy2LGdRC9Mycm1Y_Jet2OPALOmyVLrTR6IZzYml1B0GgArokbq";
 						a1.desciption = "相关描述可点击链接获得";
-						PicArticle a2 = new PicArticle();
-						a2.title = "丁香女性资讯平台";
-						a2.picUrl = "http://ec8.images-amazon.com/images/I/71i7rL8F46L._SL500_AA300_.png";
-						a2.url = "http://www.baidu.com/link?url=PQul1MKBtDqPgHh0qaQBv2HKRUeS0k4b2sYuEy2LGdRC9Mycm1Y_Jet2OPALOmyVLrTR6IZzYml1B0GgArokbq";
-						a2.desciption = "相关描述可点击链接获得";
 						List<PicArticle> articleList = new ArrayList();
 						articleList.add(a1);
-						articleList.add(a2);
 						response.articleList = articleList;
 						response.articleCount = articleList.size();
 						System.out.println(response);
