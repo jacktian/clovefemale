933b7941627084a5023e165fc2d31ab991a79e14
boxizen <boxizen@gmail.com>
2015-07-13 14:30:54 +0800

    修改提示信息

--- app/jobs/Bootstrap.java
+++ app/jobs/Bootstrap.java
@@ -11,13 +11,13 @@ import utils.PoiUtil;
 public class Bootstrap extends Job{
 	public void doJob(){
 		//获取accessToken
-		AccessTokenRefresher.getAccessToken();
+		// AccessTokenRefresher.getAccessToken();
 		//创建自定义菜单
 		//CustomMenuRefresher.createMenuNow();
 		//获取js_ticket
-		JsTicketRefresher.getJsTicket();
+		// JsTicketRefresher.getJsTicket();
 		/*	Medicine med = new Medicine();
-		med.name = "加味藿香正气丸";*/
+		// med.name = "加味藿香正气丸";*/
 		//PoiUtil.insertOneMsg();
 		//MedboxInit.init("");
 	}
